ff1b76c3a08d121ed16fcb418bdfb0640806b2de
Maria Morales <lupita.morales1@outlook.com>
2020-09-29 22:25:43 +0000

    revised files for comments and clarity

--- Sprint-1-Python_and_Descriptive_Statistics/Deliverables/campus_connections.py
+++ Sprint-1-Python_and_Descriptive_Statistics/Deliverables/campus_connections.py
@@ -1,20 +1,21 @@
 """ 
-Use enorllemt data to understand campus connetcivity
+This script uses enorllment data to understand how densely connected is the Rollins campus
     
 CMS 380, Fall 2020
-
 Maria Morales
 """
 
+# import csv module to process delimited text files
 import csv
-from more_calculations import *
+# Imports the functions from method_definitions.py to be used for distribution calculation
+from method_definitions import *
 
 # Set up matplotlib and configure it to be used on Mimir 
 import matplotlib
 matplotlib.use('Agg') # Required because we are using a remote environment
 from matplotlib import pyplot as plt
 
-# create a dictionary to store the cids for each student
+# Create a dictionary to store the cids for each student
 courses_per_student = {}
 
 # Second dictionary to store the students in each course 
@@ -34,8 +35,6 @@ for line in reader:
     r_number = line[0]
     cid = line[1]
     
-    print (r_number, cid)
-    
     # If this is the frist time we've seen the student r_number, make an entry for it in the dictionary
     if r_number not in courses_per_student:
         courses_per_student[r_number] = [] # Makes a brand new empyt lists of courses associated with this student
@@ -48,25 +47,12 @@ for line in reader:
     courses_per_student[r_number].append(cid)
     # Append the student ID so that it adds to the list of students associated with that course
     students_per_course[cid].append(r_number)
-    
-    # Add two more  statements to populate the students_per_course
-    # Reverse the roles of r-number and cid 
-#print(courses_per_student)
-
-# Iterate through the keys
-for r_number in courses_per_student:
-    print(r_number, courses_per_student[r_number])
-    
-for cid in students_per_course:
-    print("The course and the students in it are:", cid, students_per_course[cid])
-    
-
 
 # Count the total number of students in each course and append it to the list
 for cid in students_per_course:
     class_size.append(len(students_per_course[cid]))
 
-print(class_size)
+#print(class_size)
 print('The mean class size: ',calc_mean(class_size))
 print('The median class size: ', calc_median(class_size))
 
@@ -74,7 +60,7 @@ print('The median class size: ', calc_median(class_size))
 plt.figure()
 
 # Plot a histogram of the data of total students per course 
-plt.hist(class_size, 25)
+plt.hist(class_size, 20)
 
 # Set title and axes labels 
 plt.title('Class size distribution')
@@ -96,4 +82,54 @@ plt.title('Class size')
 # Save the figure to a file
 plt.savefig('class_size_boxplot.pdf', bbox_inches='tight')
 
+# INTERACTIONS
+
+student_interactions = []
+total_interactions = []
+
+# For each student look up their  list of courses
+for r_number in courses_per_student:
+    course_list = courses_per_student[r_number]
+    # Acess each course
+    for course in course_list:
+        student_list = students_per_course[course]
+        
+        # Iterate through the students in each course and keep track of unique student ids 
+        for student in student_list:
+            if student not in student_interactions:
+                student_interactions.append(student)
+                
+    total_interactions.append(len(student_interactions) - 1)
+    student_interactions.clear()
+    
+
+print('The mean student interactions: ', calc_mean(total_interactions))
+
+# Create a new figure for the histogram
+plt.figure()
+
+# Plot a histogram of the unique student interactions
+plt.hist(total_interactions, 20)
+
+# Set title and axes labels 
+plt.title('Unique student interactions')
+plt.xlabel('Unique students')
+plt.ylabel('Count')
+
+# Save the figure to a file
+plt.savefig('unique_student_histogram.pdf', bbox_inches='tight')
+
+# Create a new figure for the  boxplot
+plt.figure()
+
+# Plot the boxplot of the unique student interactions
+plt.boxplot(total_interactions)
+
+# Set title
+plt.title('Unique Student Interactions')
+
+# Save the figure to a file
+plt.savefig('unique_student_interactions_boxplot.pdf', bbox_inches='tight')
+
+            
 
