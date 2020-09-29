1ae40f77992e3fe490b03854a29ac72b33deaf1d
Fritz <fstapferpaz@rollins.edu>
2020-09-29 01:44:29 +0000

    Submission for Sprint 1 21:43 09/28/2020

--- Sprint-1-Python_and_Descriptive_Statistics/Deliverables/more_calculations.py
+++ Sprint-1-Python_and_Descriptive_Statistics/Deliverables/more_calculations.py
@@ -9,82 +9,84 @@ Calculate and print the mean, median, variance, and standard deviation.
 Save (as PDFs) a box plot and a 20-bin histogram calculated from the values.
 """
 
+"""
+Importing
+"""
 from math import sqrt               # Import Square Root for Standard Deviation Calculation
-# Required setup for matplotlib
-import matplotlib
+import matplotlib                   # Required setup for matplotlib
 matplotlib.use('Agg')               # <-- Required on Mimir
 from matplotlib import pyplot as plt
 
 
-# Add code here to open the file
+"""
+Open the File
+"""
 f = open('data.txt', 'r')           # Create file from data.txt 
-data = f.readlines()                # Read lines of file and put into array
+data = f.readlines()                # Read lines of file and put into list
 
 
-# Declare an empty list
-values = []
+"""
+Declare and populate an empty list values
+"""
+values = []                         # Declaration of list
 
-
-# Populate values from data
-for line in data:
+for line in data:                   # Populate values from data
     line = line.strip()             # Remove Whitespace
-    values.append(float(line))      # Cast Line to float and append to values array
+    values.append(float(line))      # Cast Line to float and append to values list
 
-
-# Calculation Helper Functions
 
 """
-Calculate and return the mean of input list x
+Calculation helper functions
 """
-def mean(x):
-    
+def mean(x):                        # Calculate and return the mean of input list x
     return sum(x) / len(x)          # Return Mean (Sum / Length)
 
-"""
-Calculate and return the median of input list x
-"""
-def median(x):
+def median(x):                      # Calculate and return the median of input list x
     x.sort();                       # Sort x
-    
-    # If x has even length, average both middle elements
-    if(len(x) % 2 == 0): 
+    if(len(x) % 2 == 0):            # If x has even length, average both middle elements
         e_1 = x[int(len(x) / 2)]    # First Middle Element
         e_2 = x[int(len(x)/2 -1)]   # Second Middle Element
         e_sum = e_1 + e_2           # Add middle two elements
-
         return (e_sum / 2)          #Return Average of the Middle Elementss
     else:
         e = x[ int(len(x)/2)]       # Middle Element
         return (e)                  # Return Middle Element
 
-"""
-Calculate and return the variance of input list x
-""" 
-def variance(x):
+def variance(x):                    # Calculate and return the variance of input list x
     m = mean(x)                     # Take the Mean
     sq = [(i-m)**2 for i in x]      # Uses list comprehension to get distance from mean
     
     return(sum(sq) / len(sq))       # Return Variance
     
-"""
-Calculate and return the standard deviation of input list x
-""" 
-def standard_deviation(x):
+def standard_deviation(x):          # Calculate and return the standard deviation of input list x
     return (sqrt(variance(x)))      # Return Standard Deviation (Square Root of Variance)
 
 
-# Now you calculate answers using the values list
+"""
+Now you calculate answers using the values list
+"""
 data_mean = mean(values)
 data_median = median(values)
 data_variance = variance(values)
 data_standard_deviation = standard_deviation(values)
 
 
-# NatPlotLib stuff goes here
-
-plt.figure()
+"""
+MatPlotLib stuff goes here
+Noah figured out how to put two plots in a single pdf, so I am using his code for this
+"""
+plt.subplot(121)                                # First Subplot
 plt.boxplot(values)
 plt.title("Data.txt Box-Plot")
 plt.xlabel("Data")
 plt.ylabel("Values")
+
+plt.subplot(122)                                # Second Subplot
+plt.hist(values,20)
+plt.title("Data.txt Histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+
 plt.savefig("more_calculations.pdf",bbox_inches="tight")
+
+
