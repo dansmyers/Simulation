e98e573ae7c67acc7b5a55a9ac8107295fe2b9e1
Fritz <fstapferpaz@rollins.edu>
2020-10-16 14:10:13 +0000

    Prepare to merge for Sprint 4

---
+++ Sprint-2-Probability/birthday_attack.py
@@ -0,0 +1,7 @@
+total = 1;
+for x in range(40):
+    a = ( 365 - x) / 365
+    total *= a
+    print(x)
+
+print(total)
---
+++ Sprint-2-Probability/chocolate_frogs.py
@@ -0,0 +1,5 @@
+total = 0
+for x in range(30):
+    total += 30/(x+1)
+    
+print(total)
