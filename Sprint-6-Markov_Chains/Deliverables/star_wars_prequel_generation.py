12c5396a9aa5bbabf2a9d11b9f27758772deefca
gebe678 <lehrerg6@gmail.com>
2020-12-05 21:45:52 +0000

    modified sprint 6 deliverable

--- Sprint-6-Markov_Chains/Deliverables/star_wars_prequel_generation.py
+++ Sprint-6-Markov_Chains/Deliverables/star_wars_prequel_generation.py
@@ -14,7 +14,15 @@ with open("star_wars_episode_3_script.txt") as f:
 	text = f.read()
 model_c = markovify.Text(text, state_size=2)
 
-model_combo = markovify.combine([model_a, model_b, model_c], [1.0, 20.0, 400.0])
+with open("art_of_war.txt") as f:
+	text = f.read()
+model_d = markovify.Text(text, state_size=2)
+
+with open("the_sound_and_the_fury.txt") as f:
+	text = f.read()
+model_e = markovify.Text(text, state_size=2)
+
+model_combo = markovify.combine([model_a, model_b, model_c, model_d, model_e], [3.0, 3.0, 3.0, 5.0, 5.0])
 
 # Print five randomly-generated sentences
 for i in range(10):
