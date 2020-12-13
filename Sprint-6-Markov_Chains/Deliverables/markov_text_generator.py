317a110ed14e866ec9978ab5725a775eb2d592ac
mariahhaskell <mhaskell13@gmail.com>
2020-12-13 22:05:39 +0000

    Finished

---
+++ Sprint-6-Markov_Chains/Deliverables/markov_text_generator.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.insert(0, "/usr/local/lib/python3.6/dist-packages/")
+import markovify
+
+# Get raw text as string.
+with open("taylor_lyrics.txt") as f:
+    text = f.read()
+    
+# Build the Markov model
+model = markovify.Text(text, state_size=2)
+
+# Print randomly-generated sentences
+for i in range(100):
+    print(model.make_sentence())  # try to make sentences of no more than 60 characters
+    print()
