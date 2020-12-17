7f5f0d829eaea4b01a29c6607c21876fb2911981
CHuber00 <46801480+CHuber00@users.noreply.github.com>
2020-12-17 17:39:48 +0100

    Update make_own_predictions.py

--- Sprint-6-Markov_Chains/Deliverables/Submission/make_own_predictions.py
+++ Sprint-6-Markov_Chains/Deliverables/Submission/make_own_predictions.py
@@ -7,6 +7,7 @@ with open("dr_seuss.txt") as f:
     text = f.read()
     
 # Build the Markov model
+# Tried with state_size = 3, but outputs were usually just the original sentences. 2 works best
 model = markovify.Text(text, state_size=2)
 
 # Print randomly-generated sentences
